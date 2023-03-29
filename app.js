d091b5f2d7128c8ece2abbc696ac86d583d51494
EmFey <manufety15@gmail.com>
2023-03-29 11:45:02 +0200

    Made book objects and pushed to Array

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, read){
 	this.title = title;
@@ -10,9 +10,17 @@ function Book(title, author, pages, read){
 	}
 }
 
-function addBookToLibrary() {
+const book1 = new Book("Click Millionaires", "Scott Fox", 288, false);
+const book2 = new Book("The 48 Laws of Power", "Robert Greene", 480, true);
+const book3 = new Book("Innovators", " Walter Isaacson", 480, true);
+
+myLibrary.push(book1, book2, book3);
+
+/*function addBookToLibrary() {
     // code
-  }
+}*/
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+
+console.log(myLibrary)
 console.log(theHobbit.info());
